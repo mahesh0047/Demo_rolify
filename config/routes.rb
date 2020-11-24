51a2d95ee3ab713f0669f7cc22ded563b03627e9
mahesh0047 <mahesh.tagline@gmail.com>
2020-11-24 10:03:34 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  root to: 'users#index'
+  resources :users
+  resources :addresses
+  resources :companies
+  resources :cities
+  resources :states
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  devise_scope :user do
+    get '/get_city', to: 'users/registrations#get_city'
+    # get 'check_active', to: "devise/sessions#check_active"
+  end
+
+end
